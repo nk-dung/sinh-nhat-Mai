f079d1dccae3d560b62f3dab16f85cf16af7b84e
Nguyen Khac Dung <113516151+nk-dung@users.noreply.github.com>
2022-12-17 01:42:53 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = '18-12-2000';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Happy Birthday ';
+const titleCard = 'NGUYỄN TUYẾT MAI';
+const contentCard = 'Hẹp bi bớt đây tu you. Mai bây giờ đã 23 tủi, chúc bạn Mai có 1 sinh nhật vui vẻ lúc nào cũng cười nhìu nha. Công việc thì lúc nào cũng thuận lợi sớm được tăng lương thưởng tết nha. Sẽ có món quà thứ 2 bất ngờ cho bạn 🎂🎂💐💐';
+
+
+const giftImage = 'hot-girl.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
